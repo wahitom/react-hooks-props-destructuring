839a12ba327f19d1adfed7a230cb5c69a8ab8347
Miriam Wahito Maina <miriammaina51@gmail.com>
2023-10-26 21:52:51 +0300

    COmpleted

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,13 +1,9 @@
 import React from "react";
 
-function MovieCard({
-  title,
-  posterSrc = "https://m.media-amazon.com/images/M/MV5BOTJjNzczMTUtNzc5MC00ODk0LWEwYjgtNzdiOTEyZmQxNzhmXkEyXkFqcGdeQXVyNzMzMjU5NDY@._V1_UY268_CR1,0,182,268_AL_.jpg",
-  genres,
-}) {
+function MovieCard({ title, poster, genres }) {
   return (
     <div className="movie-card">
-      <img src={posterSrc} alt={title} />
+      <img src={poster} alt={title} />
       <h2>{title}</h2>
       <small>{genres.join(", ")}</small>
     </div>
---
+++ src/components/SocialMedia.js
@@ -0,0 +1,13 @@
+/* function SocialMedia({ socialLinks }) {
+  const { github, linkedin } = socialLinks;
+
+  return (
+    <div>
+      <a href={github}>{github}</a>
+      <a href={linkedin}>{linkedin}</a>
+    </div>
+  );
+}
+
+export default SocialMedia;
+ */
